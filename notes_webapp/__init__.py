5abe94bac6e3c876d88e4c03d12e40f3f3a5d5aa
Chris-Emma <chrisemmaofficial@gmail.com>
2024-11-13 12:33:28 +0000

    project setup

--- notes_webapp/__init__.py
+++ notes_webapp/__init__.py
@@ -4,4 +4,10 @@ def create_app():
     app = Flask(__name__) #defines name of the application
     app.config['SECRET_KEY'] = 'saint' #encrypts or secures cookies and session data
     
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
     return app
